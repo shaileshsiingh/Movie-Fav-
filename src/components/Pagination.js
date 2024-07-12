a620553279cc30f959453b41ed52ca67e254aff9
Shailesh Singh <shaileshsiingh@gmail.com>
2024-07-12 19:11:44 +0530

    fixed the fav page

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,22 +1,19 @@
-import React, {useState} from 'react'
+import React from 'react';
 
-function Pagination({pageProp,goAhead,goBack}) {
-
-
+function Pagination({ pageProp, goBack, goAhead }) {
   return (
-    <div className='w-full flex justify-center mb-8 '>
-        <button className='p-2 border-2 border-indigo-500 text-indigo-500 border-r-0 rounded-l-xl'
-         onClick={goBack}
-         >Previous</button>
-        <button className='p-2 border-2 border-indigo-500 text-indigo-500 bg-gray-300'>
-          {pageProp}
-          </button>
-        <button className='p-2 border-2 border-indigo-500 text-indigo-500 border-l-0 rounded-r-xl'
-         onClick={goAhead}
-         >Next</button>
-        
+    <div className='flex justify-center m-4'>
+      <button onClick={goBack} className='p-2 border-2 border-gray-400 m-1 text-gray-700 rounded'>
+        Previous
+      </button>
+      <div className='p-2 border-2 border-gray-400 m-1 text-gray-700 rounded'>
+        {pageProp}
+      </div>
+      <button onClick={goAhead} className='p-2 border-2 border-gray-400 m-1 text-gray-700 rounded'>
+        Next
+      </button>
     </div>
-  )
+  );
 }
 
-export default Pagination
+export default Pagination;
